cf32503a8be316ca8124e5cfc4d196af0c563e68
Phuc Dong <dongvuminhphuc@gmail.com>
2022-09-08 00:53:43 +0700

    Added NewsCard

---
+++ src/components/Home/FeaturedNews.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import NewsCard from './NewsCard';
+import "./FeaturedNews.css";
+
+const FeaturedNews = () => {
+  return (
+    <section>
+        <div className="container featured-news__container">
+            <h2>Featured News</h2>
+
+            <div className="featured-news__news-cards">
+                <NewsCard />
+                {/* <NewsCard />
+                <NewsCard /> */}
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default FeaturedNews;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 // import Header from "./components/Header/Header";
 import Advertisement from "./Advertisement";
+import FeaturedNews from './FeaturedNews';
 
 const Home = () => {
   return (
-    <Advertisement />
+    <section>
+      <Advertisement />
+      <FeaturedNews />
+    </section>
   )
 }
 
