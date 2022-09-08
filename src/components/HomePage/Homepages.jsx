cf36909a851f6a1879842f49e5c4f8b027c4482b
Light1603 <59634280+Light1603@users.noreply.github.com>
2022-09-08 15:32:18 +0700

    Add files via upload

---
+++ src/components/HomePage/Homepages.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import FeaturedNews from "./featuredNews/FeaturedNews"
+import OtherNews from "./otherNews/OtherNews"
+
+const Homepages = () => {
+  return (
+    <>
+      <FeaturedNews />
+      <OtherNews />
+    </>
+  )
+}
+
+export default Homepages
