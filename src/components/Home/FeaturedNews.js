dc90ca735e0e4f041a679252e5a5b3c5299d5e23
Phuc Dong <dongvuminhphuc@gmail.com>
2022-09-08 12:28:09 +0700

    Added Other News

--- src/components/Home/FeaturedNews.js
+++ src/components/Home/FeaturedNews.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import NewsCard from './NewsCard';
+import FeaturedNewsCard from './FeaturedNewsCard';
 import "./FeaturedNews.css";
 
 const FeaturedNews = () => {
   return (
-    <section>
+    <section className="featured-news">
         <div className="container featured-news__container">
             <h2>Featured News</h2>
 
             <div className="featured-news__news-cards">
-                <NewsCard />
-                <NewsCard />
+                <FeaturedNewsCard />
+                <FeaturedNewsCard />
             </div>
         </div>
     </section>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 // import Header from "./components/Header/Header";
 import Advertisement from "./Advertisement";
 import FeaturedNews from './FeaturedNews';
+import OtherNews from './OtherNews';
 
 const Home = () => {
   return (
     <section>
       <Advertisement />
       <FeaturedNews />
+      <OtherNews />
     </section>
   )
 }
