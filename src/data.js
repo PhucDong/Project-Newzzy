f16ef3ff8f97321a550ac2ca8d1820d54ee8dc86
Light1603 <59634280+Light1603@users.noreply.github.com>
2022-09-08 15:30:48 +0700

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,170 @@
+export const featured = [
+    {
+      id: 1,
+      cover: "./images/hero/hero1.jpg",
+      category: "featured",
+      title: "The decreasing popularity of HDD",
+      authorName: "Nguyen Duy Quang",
+      time: "05 Sep - 2022",
+      desc: [
+        {
+          para1: "Here is paragraph 1 of the content page.",
+        },
+        {
+          para2: "Here is paragraph 2 of the content page.",
+        },
+        {
+          para3: "Here is paragraph 3 of the content page.",
+        },
+      ],
+      details: [
+        {
+          title: "This is the title of the second part of the article",
+        },
+        { para1: "Here is paragraph 1 of the content page." },
+        {
+          quote: "Here is the quote of the content page.",
+        },
+        {
+          para2: "Here is paragraph 2 of the content page.",
+        },
+        {
+          para3: "Here is paragraph 3 of the content page.",
+        },
+      ],
+    },
+    {
+      id: 2,
+      cover: "./images/hero/hero2.jpg",
+      category: "sport",
+      title: "More than billion football fans attend Brazil world cup",
+      authorName: "alex",
+      time: "05 Sep - 2022",
+      desc: [
+        {
+          para1: "Here is paragraph 1 of the content page.",
+        },
+        {
+          para2: "Here is paragraph 2 of the content page.",
+        },
+        {
+          para3: "Here is paragraph 3 of the content page.",
+        },
+      ],
+      details: [
+        {
+          title: "This is the title of the second part of the article",
+        },
+        { para1: "Here is paragraph 1 of the content page." },
+        {
+          quote: "Here is the quote of the content page.",
+        },
+        {
+          para2: "Here is paragraph 2 of the content page.",
+        },
+        {
+          para3: "Here is paragraph 3 of the content page.",
+        },
+      ],
+    },
+    {
+      id: 3,
+      cover: "./images/hero/hero3.jpg",
+      category: "fashion",
+      title: "No escaping new high tech speed cameras",
+      authorName: "rina",
+      time: "05 Sep - 2022",
+      desc: [
+        {
+          para1: "Here is paragraph 1 of the content page.",
+        },
+        {
+          para2: "Here is paragraph 2 of the content page.",
+        },
+        {
+          para3: "Here is paragraph 3 of the content page.",
+        },
+      ],
+      details: [
+        {
+          title: "This is the title of the second part of the article",
+        },
+        { para1: "Here is paragraph 1 of the content page." },
+        {
+          quote: "Here is the quote of the content page.",
+        },
+        {
+          para2: "Here is paragraph 2 of the content page.",
+        },
+        {
+          para3: "Here is paragraph 3 of the content page.",
+        },
+      ],
+    },
+    {
+      id: 4,
+      cover: "./images/hero/hero4.jpg",
+      category: "technology",
+      title: "Emma Watson stands up for Turkish women",
+      authorName: "sunil",
+      time: "05 Sep - 2022",
+      desc: [
+        {
+          para1: "Here is paragraph 1 of the content page.",
+        },
+        {
+          para2: "Here is paragraph 2 of the content page.",
+        },
+        {
+          para3: "Here is paragraph 3 of the content page.",
+        },
+      ],
+      details: [
+        {
+          title: "This is the title of the second part of the article",
+        },
+        { para1: "Here is paragraph 1 of the content page." },
+        {
+          quote: "Here is the quote of the content page.",
+        },
+        {
+          para2: "Here is paragraph 2 of the content page.",
+        },
+        {
+          para3: "Here is paragraph 3 of the content page.",
+        },
+      ],
+    },
+  ]
+
+  export const other = [
+    {
+      id: 1,
+      category: "world",
+      title: "China's Real estate bubble close to explosion",
+      date: "06. September 2022",
+      cover: "./imgages/otherNews/other1.jpg",
+    },
+    {
+      id: 2,
+      category: "world",
+      title: "China's Real estate bubble close to explosion",
+      date: "06. September 2022",
+      cover: "./imgages/otherNews/other1.jpg",
+    },
+    {
+      id: 3,
+      category: "world",
+      title: "China's Real estate bubble close to explosion",
+      date: "06. September 2022",
+      cover: "./imgages/otherNews/other1.jpg",
+    },
+    {
+      id: 4,
+      category: "world",
+      title: "China's Real estate bubble close to explosion",
+      date: "06. September 2022",
+      cover: "./imgages/otherNews/other1.jpg",
+    },
+  ]
+
