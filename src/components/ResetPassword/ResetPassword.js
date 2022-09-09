d6802e42159186d5900874ac4b4bd98e9eb3d18a
Phuc Dong <dongvuminhphuc@gmail.com>
2022-09-09 11:18:03 +0700

    Changed Forgot to Reset

--- src/components/ResetPassword/ResetPassword.js
+++ src/components/ResetPassword/ResetPassword.js
@@ -7,7 +7,7 @@ const ResetPassword = () => {
     <>
         <Header />
         <div className="signUpForm logInForm resetPasswordForm">
-            <h2>FORGOT PASSWORD</h2>
+            <h2>RESET PASSWORD</h2>
             <p>Forgot your password? Enter your email address and we’ll send you a link to create a new one.</p>
 
             <input 
