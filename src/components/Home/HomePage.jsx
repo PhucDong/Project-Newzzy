53494482e3b6bc41cc8bc5128030151cbc88d432
Phuc Dong <dongvuminhphuc@gmail.com>
2022-09-10 14:31:46 +0700

    Changed margin

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -5,7 +5,7 @@ import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <main>
+    <main className="main-content">
       <div className="container">
         <FeaturedNews />
         <OtherNews />
