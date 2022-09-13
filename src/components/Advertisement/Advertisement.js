1a0ef738f48c04bf193921985e811560083336a7
Phuc Dong <dongvuminhphuc@gmail.com>
2022-09-13 14:59:48 +0700

    Updated advertisement section

--- src/components/Advertisement/Advertisement.js
+++ src/components/Advertisement/Advertisement.js
@@ -6,6 +6,7 @@ const Advertisement = () => {
     <section className="advertisement">
       <div className="container advertisement__container">
         <h2>Quarterly Sale 2022</h2>
+        <p className="advertisement__category">Advertisement</p>
         <p>10% discount for all advertising services</p>
         <p>8 September - 21 September 2022</p>
       </div>
